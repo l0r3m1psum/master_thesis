7eab8b62d9e2a3c58bad736880cfd767f8821cd9
l0r3m1psum <bellanidiego@gmail.com>
2024-01-29 12:22:04 +0100

    I've tried to use a better numerical differentiation... Failing.

--- numerical_diff.py
+++ numerical_diff.py
@@ -22,3 +22,17 @@ grad2 = (f(x1, x2 + h, x3, x4) - fX)/h
 grad3 = (f(x1, x2, x3 + h, x4) - fX)/h
 grad4 = (f(x1, x2, x3, x4 + h) - fX)/h
 print(grad1, grad2, grad3, grad4)
+
+# https://nbviewer.org/urls/draft.ocw.mit.edu/courses/18-s096-matrix-calculus-for-machine-learning-and-beyond-january-iap-2023/fd_checks.ipynb#Finite-difference-approximations:-Easy-version
+def g(A):
+	B = numpy.ones((2,2))*2
+	C = A@B
+	return C
+
+numpy.random.seed(42)
+A = numpy.random.randn(2, 2)
+# A = numpy.ones((2,2))
+dA = numpy.random.randn(2, 2) * 1e-8
+approx = g(A + dA) - g(A)
+print(approx/dA)
+print(numpy.ones((2,2))@(approx/dA).T)
